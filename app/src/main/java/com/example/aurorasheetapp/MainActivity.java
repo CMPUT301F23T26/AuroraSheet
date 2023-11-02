8011a260d38f8d0f17f561a9c7c41ff0a91fb135
mehreenpa <90921445+mehreenpa@users.noreply.github.com>
2023-11-02 17:44:21 -0600

    registration and login layout done

--- app/src/main/java/com/example/aurorasheetapp/MainActivity.java
+++ app/src/main/java/com/example/aurorasheetapp/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.aurorasheetapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-// test
 public class MainActivity extends AppCompatActivity {
 
 
+
+
 }
-
-
