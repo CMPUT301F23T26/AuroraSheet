a48d3067d570a74b37048f097d4d153113333b95
pangstar019 <panger19@yahoo.com>
2023-11-07 14:00:10 -0700

    Tags done

--- app/src/main/java/com/example/aurorasheetapp/MainActivity.java
+++ app/src/main/java/com/example/aurorasheetapp/MainActivity.java
@@ -4,58 +4,89 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 /**
  * This class serves as the main activity and manages a list of Item Records.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        RecyclerViewInterface,
+        TagFragment.OnFragmentInteractionListener {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<Item> listItems;
 
     private TextView totalAmountTextView;
     private FloatingActionButton addButton;
+    private FloatingActionButton editButton;
+    private FloatingActionButton deleteButton;
+
+    private RecyclerView tagView;
+    private RecyclerView.Adapter tagAdapter;
+    private ArrayList<Tag> tags; // keeps track of all tags
+    private ArrayList<Tag> selected_tags; // keeps track of tags to display items
+    private Tag selected_tag;
+    private FloatingActionButton addTag_btn;
+
+    private ImageButton profile_btn;
+    private ImageButton sort_btn;
+    private ImageButton search_btn;
+
+    private FirebaseFirestore firestore;
+
+    private int itemIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //access database
+        firestore = FirebaseFirestore.getInstance();
+        loadItemsFromFirestore();
+
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         listItems = new ArrayList<>();
-// just for texting you can delete later
-        for (int i=0;i<10;i++){
-            Item listItem = new Item(
-                    +i+1,
-                    "good " +i+1,
-                    "wooden "+i+1,
-                    20 + i+1,
-                    "tfd "+ i+1,
-                    13 +i+1,
-                    "nahh "+i+1
-            );
-            listItems.add(listItem);
 
-        }
-        adapter = new CustomArrayAdapter(listItems,this);
+        adapter = new CustomArrayAdapter(listItems, this);
         recyclerView.setAdapter(adapter);
 
         totalAmountTextView = findViewById(R.id.totalValue);
         addButton = findViewById(R.id.buttonAdd);
+        editButton = findViewById(R.id.buttonEdit);
+        deleteButton = findViewById(R.id.buttonDelete);
+
+        tagView = findViewById(R.id.tag_View);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        tagView.setLayoutManager(layoutManager);
+        listItems = new ArrayList<>();
+        tags = new ArrayList<>();
+        selected_tags = new ArrayList<>();
+        selected_tag = null;
 
         // navigate to the add item activity on click of the add button
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -66,21 +97,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                launchEditData(intent, itemIndex);
+            }
+        });
         // display total value for all the items
         totalAmountTextView = findViewById(R.id.totalValue);
         totalAmountTextView.setText(computeTotal());
-    }
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (itemIndex > -1) {
+                    listItems.remove(itemIndex);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            handleAddItemResult(data);
+        for (int i = 1; i < 11; i++){
+            Tag tag = new Tag("Tag"+i);
+            tags.add(tag);
         }
+
+        tagAdapter = new CustomTagAdapter(tags, this,
+                new CustomTagAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(Tag tag) {
+                        boolean status = tag.getStatus();
+                        if (status){
+                            tag.unselect_tag();
+                        } else {
+                            tag.select_tag();
+                        }
+                        tagAdapter.notifyDataSetChanged();
+                    }
+                },
+                new CustomTagAdapter.OnItemLongClickListener() {
+                    @Override
+                    public void onItemLongClick(Tag tag) {
+                        new TagFragment(tag).show(getSupportFragmentManager(), "edit_tag");
+                    }
+                });
+        tagView.setAdapter(tagAdapter);
+        addTag_btn = findViewById(R.id.addTagButton);
+        profile_btn = findViewById(R.id.userProfile_btn);
+        sort_btn = findViewById(R.id.sortItem_btn);
+        search_btn = findViewById(R.id.searchItem_btn);
+
+        addTag_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected_tag = null;
+                new TagFragment(selected_tag).show(getSupportFragmentManager(), "add_tag");
+            }
+        });
     }
 
 
-    // move this to item list class?
+
+    // move computeTotal to item list class?
+    /**
+     * Computes the total value of all the items in the list.
+     * @return A string representation of the total value of all the items.
+     */
     public String computeTotal() {
         double total = 0;
         for (Item item : listItems) {
@@ -89,22 +171,41 @@ public class MainActivity extends AppCompatActivity {
         return String.format("%.2f", total);
     }
 
+    private void launchEditData(Intent intent, int i) {
+        if (intent != null) {
+            Item itemToEdit = listItems.get(i);
+            intent.putExtra("name", itemToEdit.getName());
+            intent.putExtra("value", itemToEdit.getEstimatedValue());
+            intent.putExtra("time", itemToEdit.getDateOfPurchase().toString());
+            intent.putExtra("make", itemToEdit.getMake());
+            intent.putExtra("comment", itemToEdit.getComment());
+            intent.putExtra("model", itemToEdit.getModel());
+            intent.putExtra("serial", itemToEdit.getSerialNumber());
+            intent.putExtra("description", itemToEdit.getBriefDescription());
+            intent.putExtra("index", i);
+            editItemLauncher.launch(intent);
+        }
+    }
+
     private void handleAddItemResult(Intent data) {
         if (data != null) {
             String name = data.getStringExtra("name");
             String description = data.getStringExtra("description");
+            ItemDate date = new ItemDate(data.getStringExtra("date"));
             String value = data.getStringExtra("value");
+            String serial = data.getStringExtra("serial");
             String make = data.getStringExtra("make");
             String model = data.getStringExtra("model");
             String comment = data.getStringExtra("comment");
 
             Item listItem = new Item(
-                    1,
                     name,
+                    date,
                     description,
-                    Integer.parseInt(value),
                     make,
-                    1,
+                    Integer.parseInt(serial),
+                    model,
+                    Integer.parseInt(value),
                     comment
             );
             listItems.add(listItem);
@@ -113,6 +214,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void EditItemResult(Intent data) {
+        if (data != null) {
+            Boolean isDelete = data.getBooleanExtra("isDelete", false);
+            if (isDelete) {
+                int index = data.getIntExtra("index", -1);
+                if (index > -1) {
+                    listItems.remove(index);
+                    adapter.notifyDataSetChanged();
+                    totalAmountTextView.setText(computeTotal());
+                }
+            } else {
+                String name = data.getStringExtra("name");
+                String description = data.getStringExtra("description");
+                String value = data.getStringExtra("value");
+                String make = data.getStringExtra("make");
+                String model = data.getStringExtra("model");
+                String comment = data.getStringExtra("comment");
+                ItemDate date = new ItemDate(data.getStringExtra("time"));
+                Double serial = Double.parseDouble(data.getStringExtra("serial"));
+                int index = data.getIntExtra("index", -1);
+
+                if (index != -1) {
+                    Item item = listItems.get(index);
+                    item.setMake(make);
+                    item.setComment(comment);
+                    item.setName(name);
+                    item.setEstimatedValue(Double.parseDouble(value));
+                    item.setModel(model);
+                    item.setDateOfPurchase(date);
+                    item.setBriefDescription(description);
+                    item.setSerialNumber(serial);
+                }
+                adapter.notifyDataSetChanged();
+                totalAmountTextView.setText(computeTotal());
+            }
+        }
+    }
 
     private final ActivityResultLauncher<Intent> addItemLauncher =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
@@ -124,5 +262,96 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     });
+    private final ActivityResultLauncher<Intent> editItemLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == 1) {
+                            Intent data = result.getData();
+                            if (data != null) {
+                                EditItemResult(data);
+                            }
+                        }
+                    });
+
+    @Override
+    public void onItemClick(int position) {
+        itemIndex = position;
+
+        //shows two buttons once clicked
+        editButton.setVisibility(View.VISIBLE);
+        deleteButton.setVisibility(View.VISIBLE);
+
+    }
+    //i added the following to access database and clear lisst of items and only display the ones in the database
+    private void loadItemsFromFirestore() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser == null) {
+            Toast.makeText(this, "No user logged in", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firestore.collection("users")
+                .document(currentUser.getUid())
+                .collection("items")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        listItems.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Log.d("Firestore", document.getId() + " => " + document.getData());
+
+                            Item item = new Item(
+                                    document.getString("name"),
+                                    new ItemDate(document.getString("date")),
+                                    document.getString("description"),
+                                    document.getString("make"),
+                                    Double.parseDouble(document.getString("serial")),
+                                    document.getString("model"),
+                                    Double.parseDouble(document.getString("value")),
+                                    document.getString("comment")
+                            );
+
+                            listItems.add(item);
+                        }
+                        adapter.notifyDataSetChanged();
+                        totalAmountTextView.setText(computeTotal());
+                    } else {
+                        Log.w("Firestore", "Error getting documents.", task.getException());
+                        Toast.makeText(MainActivity.this, "Error getting items.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    // Override interface methods for add tag fragment
+    @Override
+    public void onCancelPressed() {
+        selected_tag = null;
+    }
+
+    @Override
+    public void onOkPressed(Tag newTag, Tag old_tag, Boolean editing) {
+        // is editing a selected expense
+        if (editing){
+            //find the index within ArrayList
+            Integer index = tags.indexOf(old_tag);
+            // set new expense information to current index
+            tags.set(index, new Tag(newTag.getName()));
+            tagAdapter.notifyDataSetChanged();
+            // adding a new expense
+        } else if (!Objects.equals(newTag.getName(), "default")){
+            tags.add(newTag); // add to ArrayList
+            tagAdapter.notifyDataSetChanged();
+        }
+        selected_tag = null;
+    }
+
+    @Override
+    public void onDeletePressed(Tag tag){
+        tags.remove(tag);
+        tagAdapter.notifyDataSetChanged();
+        selected_tag = null;
+    }
+
 
 }
