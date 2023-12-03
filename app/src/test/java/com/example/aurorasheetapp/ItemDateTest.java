d3e582ee8c08a846ed03a3adbc2130bbe6a60dcb
Jeycanate <71449545+Jeycanate@users.noreply.github.com>
2023-12-03 15:14:04 -0700

    ItemDateTest done

---
+++ app/src/test/java/com/example/aurorasheetapp/ItemDateTest.java
@@ -0,0 +1,38 @@
+package com.example.aurorasheetapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ItemDateTest {
+    private ItemDate itemDate;
+    @Test
+    public void testStringRepresentation(){
+        String dateString = "11-12-2021";
+        itemDate = new ItemDate(dateString);
+        assertEquals(itemDate.toString(), dateString);
+    }
+
+    @Test
+    public void testParseDate(){
+        String dateString = "11-12-2021";
+        int[] dateInts = ItemDate.parseDate(dateString);
+        assertEquals(dateInts[0], 11);
+        assertEquals(dateInts[1], 12);
+        assertEquals(dateInts[2], 2021);
+    }
+    @Test
+    public void testConstructor(){
+        String dateString = "11-12-2021";
+        itemDate = new ItemDate(dateString);
+        assertEquals(11, itemDate.getDay());
+        assertEquals(12,  itemDate.getMonth());
+        assertEquals(2021, itemDate.getYear());
+
+        int[] dateInts = {8, 12, 2021};
+        itemDate = new ItemDate(dateInts[0], dateInts[1], dateInts[2]);
+        assertEquals(8, itemDate.getDay());
+        assertEquals(12,  itemDate.getMonth());
+        assertEquals(2021, itemDate.getYear());
+    }
+}
