5ec5b68edc40759db61e2d2bb8100d79abcab0a4
Jeycanate <71449545+Jeycanate@users.noreply.github.com>
2023-12-03 15:31:47 -0700

    Hardcoded but streamlined main activity test

--- app/src/androidTest/java/com/example/aurorasheetapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/aurorasheetapp/MainActivityTest.java
@@ -15,21 +15,26 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import java.util.UUID;
 
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MainActivityTest {
     // define rules
     @Rule
     public ActivityScenarioRule<Login> loginActivityRule =
             new ActivityScenarioRule<>(Login.class);
+    String randomItemName;
 
     @Test
-    public void testAddItemAfterLogin() {
+    public void A_testAddItemAfterLogin() {
         // Login to app
         onView(withId(R.id.user_login)).perform(ViewActions.typeText("bonobo"));
         onView(withId(R.id.password_login)).perform(ViewActions.typeText("cmput301"), ViewActions.closeSoftKeyboard());
@@ -49,7 +54,7 @@ public class MainActivityTest {
         // Click on Add Item button
         onView(withId(R.id.buttonAdd)).perform(click());
 
-        String randomItemName = UUID.randomUUID().toString();
+        String randomItemName = "TestString";
         // Enter in test data for the item and close the keyboard after each entry
         onView(withId(R.id.itemName)).perform(ViewActions.typeText(randomItemName), ViewActions.closeSoftKeyboard());
         onView(withId(R.id.itemDescription)).perform(ViewActions.typeText("Test Description"), ViewActions.closeSoftKeyboard());
@@ -71,7 +76,7 @@ public class MainActivityTest {
 
 
     @Test
-    public void testEditItem(){
+    public void B_testEditItem(){
         //login to user
         onView(withId(R.id.user_login)).perform(ViewActions.typeText("bonobo"));
         onView(withId(R.id.password_login)).perform(ViewActions.typeText("cmput301"), ViewActions.closeSoftKeyboard());
@@ -88,31 +93,11 @@ public class MainActivityTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        //add item before testing edit
-        onView(withId(R.id.buttonAdd)).perform(click());
 
-        String randomItemName = UUID.randomUUID().toString();
-        // Enter in test data for the item and close the keyboard after each entry
-        onView(withId(R.id.itemName)).perform(ViewActions.typeText(randomItemName), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemDescription)).perform(ViewActions.typeText("Test Description"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemDate)).perform(ViewActions.click());
-        onView(withText("OK")).perform(ViewActions.click());
-        onView(withId(R.id.itemValue)).perform(ViewActions.typeText("100"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemSerialNumber)).perform(ViewActions.typeText("100"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemMake)).perform(ViewActions.typeText("Test Make"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemModel)).perform(ViewActions.typeText("Test Model"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemComment)).perform(ViewActions.typeText("Test Comment"), ViewActions.closeSoftKeyboard());
-
-        onView(withId(R.id.addItemButton)).perform(click());
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         //click on the item on recycler view
         onView(withId(R.id.recyclerView)).perform(
                 RecyclerViewActions.actionOnItem(
-                        hasDescendant(withText(randomItemName)),
+                        hasDescendant(withText("TestString")),
                         click()
                 )
         );
@@ -123,18 +108,18 @@ public class MainActivityTest {
         }
         onView(withId(R.id.buttonEdit)).perform(click());
         // Enter in test data for the item
-        onView(withId(R.id.itemName_edit)).perform(ViewActions.clearText(), ViewActions.typeText("TestObject"));
+        onView(withId(R.id.itemName_edit)).perform(ViewActions.clearText(), ViewActions.typeText("TestObjects"));
 
         // Click on Confirm button
         onView(withId(R.id.confirmButton_edit)).perform(click());
 
         onView(ViewMatchers.withId(R.id.recyclerView));
         //check if change is applied
-        onView(withText("TestObject")).check(matches(isDisplayed()));
+        onView(withText("TestObjects")).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testDeletion(){
+    public void C_testDeletion(){
         onView(withId(R.id.user_login)).perform(ViewActions.typeText("bonobo"));
         onView(withId(R.id.password_login)).perform(ViewActions.typeText("cmput301"), ViewActions.closeSoftKeyboard());
 
@@ -150,32 +135,11 @@ public class MainActivityTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        //add item before deleting
-        onView(withId(R.id.buttonAdd)).perform(click());
-        String randomItemName = UUID.randomUUID().toString();
-        // Enter in test data for the item and close the keyboard after each entry
-        onView(withId(R.id.itemName)).perform(ViewActions.typeText(randomItemName), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemDescription)).perform(ViewActions.typeText("Test Description"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemDate)).perform(click());
-        onView(withText("OK")).perform(click());
-        onView(withId(R.id.itemValue)).perform(ViewActions.typeText("100"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemSerialNumber)).perform(ViewActions.typeText("100"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemMake)).perform(ViewActions.typeText("Test Make"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemModel)).perform(ViewActions.typeText("Test Model"), ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.itemComment)).perform(ViewActions.typeText("Test Comment"), ViewActions.closeSoftKeyboard());
 
-        // Click on Confirm button
-        onView(withId(R.id.addItemButton)).perform(click());
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         //click on the item on recycler view
         onView(withId(R.id.recyclerView)).perform(
                 RecyclerViewActions.actionOnItem(
-                        hasDescendant(withText(randomItemName)),
+                        hasDescendant(withText("TestObjects")),
                         click()
                 )
         );
@@ -188,6 +152,6 @@ public class MainActivityTest {
         onView(ViewMatchers.withId(R.id.recyclerView));
         onView(withId(R.id.buttonDelete)).perform(click());
         //check if deletion worked
-        onView(withText(randomItemName)).check(doesNotExist());
+        onView(withText("TestObjects")).check(doesNotExist());
     }
 }
